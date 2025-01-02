24fda87480c8935a9513c5de1014fe977776918e
SurajRaju2008 <surajraju@icloud.com>
2025-01-01 20:39:39 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 fetch('questions.json')
     .then(response => response.json()) 
+    .then(questions => {               
+        const today = new Date().toISOString().split('T')[0];
+        const todaysQuestion = questions.find(q => q.date === today);})
