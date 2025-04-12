c0dfa0bcd026b717e7aa02fa810965cb27f194fa
SurajRaju2008 <surajraju@icloud.com>
2025-04-12 13:38:25 -0500

    style

--- Main/script.js
+++ Main/script.js
@@ -114,3 +114,10 @@ function checkAnswer(selected, correctAnswer) {
   window.location.href = 'displayAnswer.html';
 }
 
+function closePopup() {
+  document.getElementById("howToPlayPopup").classList.remove("show");
+}
+
+function closePopup() {
+  document.getElementById("howToPlayPopup").classList.remove("show");
+}
